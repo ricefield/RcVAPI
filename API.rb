b8253cb3f9d0693e64a3f89f7c65cacc71fa1f6c
Jonathan Tien <jonathan.tien@gmail.com>
2013-11-21 14:07:09 -0800

    fleshed out parser, added error check, still need to calculate verse reference"

--- API.rb
+++ API.rb
@@ -23,15 +23,15 @@ get '/v/:book/:ch/:verse' do
 
 end
 
-# get a range of verses
+# get a single range of verses
 # ex: /vv/Gen/1/26/Gen/1/27
-get '/vv/:begbk/:bgch/:bgv/:endbk/:endch/:endv' do
+get '/vrange/:begbk/:bgch/:bgv/:endbk/:endch/:endv' do
 
 end
 
-# get a range of verses within the same chapter
+# get a single range of verses within the same chapter
 # ex: /vv/2Cor/3/16/18
-get '/vv/:book/:ch/:beg/:end' do
+get '/vrange/:book/:ch/:beg/:end' do
 
 end
 
@@ -43,29 +43,48 @@ get '/osis/' do
 
 end
 
-def rcv
+def get_rcv(book, chapter, verse, endbook=nil, endchapter=nil, endverse=nil)
 
     content_type :json
-    verse = Hash.new
+    data = Hash.new
 
-    PAGE_URL = "http://online.recoveryversion.org/getScripture.asp?vinfo="+:book+:ch+":"+:verse
+    if endbook.nil? or endchapter.nil? or endverse.nil?
+        PAGE_URL = "http://online.recoveryversion.org/getScripture.asp?vinfo="+book+chapter+":"+verse
+    else
+        PAGE_URL = "http://online.recoveryversion.org/getScripture.asp?vinfo="+book+chapter+":"+verse+"-"+endbook+endchapter+":"+endverse
+    end
 
     # start building json
-    verse['ref'] = :book + " " + :ch + ":" + :verse
-    verse['url'] = PAGE_URL
+    data['request'] = PAGE_URL[57..-1]  # requested verse reference(s)
+    data['url'] = PAGE_URL
+    data['verses'] = Hash.new
     
     page = Nokogiri::HTML(open(PAGE_URL))
+    ref = [].push(book, chapter).join('.')  # beginning reference (minus verse)
 
-    # remove footnotes/crossreferences
-    page.css("div#content p.verses sup").remove
+    # error-check
+    unless page.css("p.error").nil?
+        data['error'] = "Your request was invalid"
+        return data
+    end
 
-    # remove 
-    page.css("div#content p.verses b.versenum").remove
+    for verse in page.css("p.verses") do
+
+        # calculate the reference
+        verse.css("b.versenum").text
+
+        data['verses'][]
+        # remove bolded verse number
+        verse.css("b.versenum").remove
+        # remove superscripts (footnotes+cross references)
+        verse.css("sup").remove
+        data['verses'][]
+
 
     # yields clean verse text
-    verse['text'] = page.css("div#content p.verses").text.strip
+    data['text'] = page.css("div#content p.verses").text.strip
 
-    verse.to_json
+    return data.to_json
     
 end
 
