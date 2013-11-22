9c215020a349e7a7f9eb00b8bc0d6c77290e0ffe
Jonathan Tien <jonathan.tien@gmail.com>
2013-11-22 15:27:49 -0800

    finished up calculation of verse reference using temp variables.

--- API.rb
+++ API.rb
@@ -49,18 +49,17 @@ def get_rcv(book, chapter, verse, endbook=nil, endchapter=nil, endverse=nil)
     data = Hash.new
 
     if endbook.nil? or endchapter.nil? or endverse.nil?
-        PAGE_URL = "http://online.recoveryversion.org/getScripture.asp?vinfo="+book+chapter+":"+verse
+        url = "http://online.recoveryversion.org/getScripture.asp?vinfo="+book+chapter+":"+verse
     else
-        PAGE_URL = "http://online.recoveryversion.org/getScripture.asp?vinfo="+book+chapter+":"+verse+"-"+endbook+endchapter+":"+endverse
+        url = "http://online.recoveryversion.org/getScripture.asp?vinfo="+book+chapter+":"+verse+"-"+endbook+endchapter+":"+endverse
     end
 
     # start building json
-    data['request'] = PAGE_URL[57..-1]  # requested verse reference(s)
-    data['url'] = PAGE_URL
+    data['request'] = url[57..-1]  # requested verse reference(s)
+    data['url'] = url
     data['verses'] = Hash.new
     
-    page = Nokogiri::HTML(open(PAGE_URL))
-    ref = [].push(book, chapter).join('.')  # beginning reference (minus verse)
+    page = Nokogiri::HTML(open(url))
 
     # error-check
     unless page.css("p.error").nil?
@@ -68,21 +67,33 @@ def get_rcv(book, chapter, verse, endbook=nil, endchapter=nil, endverse=nil)
         return data
     end
 
+    # helpful for calculating references
+    ref = [].push(book, chapter, verse)  # beginning reference (minus verse)
+    prev = []
+
     for verse in page.css("p.verses") do
 
-        # calculate the reference
-        verse.css("b.versenum").text
+        # calculate the reference <ref
+        prev = ref
+        vnum = verse.css("b.versenum").text
+        # checks if this is a new chapter (as opposed to the beginning of the sequence)
+        if vnum == '1' and prev[2].to_i > vnum.to_i
+            # and if so, increment the chapter number
+            ref[1] = ref[1].to_i++
+        end
+        # update the verse number
+        ref[2] = vnum
+        ref = ref.join('.')
 
-        data['verses'][]
         # remove bolded verse number
         verse.css("b.versenum").remove
         # remove superscripts (footnotes+cross references)
         verse.css("sup").remove
-        data['verses'][]
 
+        # put text in struct
+        data['verses'][ref] = verse.css("div#content p.verses").text.strip
 
-    # yields clean verse text
-    data['text'] = page.css("div#content p.verses").text.strip
+    end
 
     return data.to_json
     
